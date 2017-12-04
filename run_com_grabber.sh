8c24a558c1f69719538398418dfd3a6321234fd4
K Goryachevskiy <kgoryachevskiy@gmail.com>
2017-12-04 17:37:07 +0300

    Simplified search of HI byte order

--- run_com_grabber.sh
+++ run_com_grabber.sh
@@ -3,4 +3,4 @@
 #ls -la /dev/ttyUSB0
 #and add yourself to the group by typing
 #sudo usermod -a G uucp my_user
-./com_grabber -p16 -b1000000 -s1024 -m8N1 -l1 -w500 -a0
+./com_grabber -p16 -b1000000 -s1000 -m8N1 -l1 -w20 -a0
